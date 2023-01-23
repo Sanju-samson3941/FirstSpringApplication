cbe649876e1b601536c9b3439cf1f14e291e236c
Sanju-samson3941 <sanjusamson3941@gmail.com>
2023-01-23 16:26:17 +0530

    added comment to used git gui

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// this is the starting point of the application
 @SpringBootApplication
 public class DemoApplication {
 
